f993d11e48a7dd6346ca487c5342c85eb5f5bf34
Matthew Cox <mattdrummerguy@gmail.com>
2022-10-12 13:42:44 -0500

    added concept

---
+++ learning.js
@@ -0,0 +1,29 @@
+//Use Comments to Break down questions
+
+/*
+Example of a question:
+1. Write a function that adds two numbers together and returns the sum  of the two numbers.
+
+Example of a question broken down manageable steps:
+Step 1: Create three variables, num1, num2, and sum.
+Step 2: Assign values to num1 and num2.
+Step 3: Add num1 and num2 together and assign the result to sum.
+Step 4: Print sum in a console.log.
+
+Once you are Done with the steps, fill in the blanks with code accordingly. This may requiring updating or changing the order of the steps.
+*/
+
+//Step 1: Create three variables, num1, num2, and sum.
+//Step 2: Assign values to num1 and num2.
+
+//Use Comments to document what each piece of your code is DOING
+let num1 = 5; //assigns the value of 5 to num1
+let num2 = 10; //assigns the value of 10 to num2
+let sum = 0; //assigns the value of 0 to sum
+
+//Step 3: Add num1 and num2 together and assign the result to sum.
+sum = num1 + num2; //adds num1 and num2 together and assigns the result to sum
+
+//Step 4: Print sum in a console.log.
+//Use Console logs to test your code, see what is happening, and describe what is happening
+console.log("This is the sum of num1 and num2:", sum); //prints the value of sum to the console
